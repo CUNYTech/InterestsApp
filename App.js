9f06a6896908f2e521e11d6f5bc723771616945b
kev-th <kevinkthaw@gmail.com>
2017-10-02 20:35:52 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+<<<<<<< HEAD
         <Text>Happy Coding!</Text>
       </View>
     );
