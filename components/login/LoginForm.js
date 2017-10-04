f1540a4c643122472c650053b5a941f243e7e9ca
kev-th <kevinkthaw@gmail.com>
2017-10-04 00:37:50 -0400

    implemented firebase for login

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -1,13 +1,65 @@
 //impo
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert, Button ,StyleSheet ,StatusBar} from 'react-native';
-
+import * as firebase from "firebase";
 const onButtonPress = () => {
   Alert.alert('Link to Auth');
 };
 
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            response: ""
+        };
+
+        this.signup = this.signup.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    async signup() {
+
+
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+
+            this.setState({
+                response: "Account Created"
+               
+            });
+
+            alert(this.state.response);
+
+        } catch (error) {
+            this.setState({
+                response: error.toString()
+            })
+        }
+
+    }
+
+    async login() {
+
+        try {
+            await firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+
+            this.setState({
+                response: "Logged In!"
+            });
+
+            alert(this.state.response)
+        } catch (error) {
+            this.setState({
+                response: error.toString()
+            })
+        }
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -19,16 +71,21 @@ class LoginForm extends Component {
                             keyboardType='email-address'
                             returnKeyType="next"
                             placeholder='Email or Mobile Num'
+                            onChangeText ={(email) => this.setState({email})}
                             placeholderTextColor='rgba(225,225,225,0.7)'/>
 
                 <TextInput style = {styles.input}
                            returnKeyType="go" ref={(input)=> this.passwordInput = input}
                            placeholder='Password'
+                           onChangeText ={(password) => this.setState({password})}
                            placeholderTextColor='rgba(225,225,225,0.7)'
                            secureTextEntry/>
                  {/*   <Button onPress={onButtonPress} title = 'Login' style={styles.loginButton} /> */}
-              <TouchableOpacity style={styles.buttonContainer} onPress={onButtonPress}>
-                    <Text  style={styles.buttonText}>LOGIN</Text>
+              <TouchableOpacity style={styles.buttonContainer} onPress={this.signup}>
+                    <Text  style={styles.buttonText}>Signup</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.login}>
+                    <Text  style={styles.buttonText}>Login</Text>
                 </TouchableOpacity>
             </View>
         );
